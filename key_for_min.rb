ccfa066e80fcf5749fab9ece83d01b7c2e440b4d
Joshua Frank <eurosewusa@gmail.com>
2020-10-28 22:45:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  return nil if name_hash.empty?
   result = name_hash.first.value
   name_hash.each do |name|
     result = name_hash[ :name ] if name_hash[ :name ] < result
