2a62fe7c4ffb169b55da5af640e23f3791399050
Joshua Frank <eurosewusa@gmail.com>
2020-10-28 22:41:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  result = name_hash.options_from_collection_for_select
+  result = name_hash.first
+  name_hash.each do |name|
+    #
+  end
+  result
 end
